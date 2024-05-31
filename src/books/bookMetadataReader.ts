ba997898506a4b5013e4c039f637c742ea54191c
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-05-31 22:40:21 +0200

    script to resolve isbn metadata with calibre

--- src/books/bookMetadataReader.ts
+++ src/books/bookMetadataReader.ts
@@ -1,4 +1,37 @@
+import { z } from 'zod'
+import { resolve } from 'node:path'
 import { spawnSync } from 'node:child_process'
+import { existsSync, readFileSync, writeFileSync } from 'node:fs'
 
-const flup = spawnSync('fetch-ebook-metadata', ['--isbn', '9780321774514'])
-console.log(flup.stdout.toString())
+const IsbnSchema = z.array(z.string())
+
+const cwd = process.cwd()
+const isbnFiles = resolve(cwd, 'cache', 'isbn-numbers.json')
+
+const calibreCacheFileFor = (base: string, isbn: string) => {
+  return resolve(base, 'cache', 'calibre', `${isbn}.txt`)
+}
+
+const calibreOutputFor = (cwd: string, isbn: string) => {
+  const cacheFile = calibreCacheFileFor(cwd, isbn)
+  if (existsSync(cacheFile)) {
+    console.log('cache hit')
+    return readFileSync(cacheFile)
+  }
+  const flup = spawnSync('fetch-ebook-metadata', ['--isbn', isbn])
+  if (flup.status !== 0) {
+    throw new Error('check what wrong')
+  }
+  const output = flup.stdout.toString()
+  writeFileSync(cacheFile, output)
+  return output
+}
+
+function readIsbnNumbers() {
+  console.log(isbnFiles)
+  const isbnAsString = readFileSync(isbnFiles).toString()
+  return IsbnSchema.parse(JSON.parse(isbnAsString))
+}
+
+const isbnNumbers = readIsbnNumbers()
+isbnNumbers.forEach((isbn) => calibreOutputFor(cwd, isbn))
