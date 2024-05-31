8576f53943c77b3b9dd7f8883fe2b5b7b93f2450
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-05-31 23:19:51 +0200

    parse metadata

--- src/books/bookMetadataReader.ts
+++ src/books/bookMetadataReader.ts
@@ -2,29 +2,32 @@ import { z } from 'zod'
 import { resolve } from 'node:path'
 import { spawnSync } from 'node:child_process'
 import { existsSync, readFileSync, writeFileSync } from 'node:fs'
+import { CalibreMetadata, parse } from './CalibreMetadata.ts'
 
 const IsbnSchema = z.array(z.string())
 
 const cwd = process.cwd()
-const isbnFiles = resolve(cwd, 'cache', 'isbn-numbers.json')
+const isbnFiles = resolve(cwd, 'cache', 'book-isbn-numbers.json')
 
 const calibreCacheFileFor = (base: string, isbn: string) => {
   return resolve(base, 'cache', 'calibre', `${isbn}.txt`)
 }
 
-const calibreOutputFor = (cwd: string, isbn: string) => {
+const calibreOutputFor = (cwd: string, isbn: string): CalibreMetadata | 'failed' => {
   const cacheFile = calibreCacheFileFor(cwd, isbn)
   if (existsSync(cacheFile)) {
-    console.log('cache hit')
-    return readFileSync(cacheFile)
+    return readFileSync(cacheFile).toString() as CalibreMetadata
   }
   const flup = spawnSync('fetch-ebook-metadata', ['--isbn', isbn])
   if (flup.status !== 0) {
-    throw new Error('check what wrong')
+    console.log(isbn, flup.status)
+    console.log(flup.stdout.toString())
+    console.log(flup.stderr.toString())
+    return 'failed'
   }
   const output = flup.stdout.toString()
   writeFileSync(cacheFile, output)
-  return output
+  return output as CalibreMetadata
 }
 
 function readIsbnNumbers() {
@@ -33,5 +36,10 @@ function readIsbnNumbers() {
   return IsbnSchema.parse(JSON.parse(isbnAsString))
 }
 
-const isbnNumbers = readIsbnNumbers()
-isbnNumbers.forEach((isbn) => calibreOutputFor(cwd, isbn))
+const all = readIsbnNumbers().map((isbn) => {
+  const calibre = calibreOutputFor(cwd, isbn)
+  const parsed = calibre === 'failed' ? 'failed' : parse(calibre)
+  return { isbn, parsed }
+})
+const summary = resolve(cwd, 'cache', 'summary.json')
+writeFileSync(summary, JSON.stringify(all, null, 2))
